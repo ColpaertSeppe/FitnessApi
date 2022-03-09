3b7b4a2f8f1dee8d84833a66a43ad35870c26264
Seppe <seppe.colpaert@student.hogent.be>
2022-03-09 14:38:59 +0100

    added tracking

--- Models/TrainingPlan.cs
+++ Models/TrainingPlan.cs
@@ -6,11 +6,19 @@
         public string Description { get; set; }
         public List<TrainingDay> TrainingDays { get; set; }
 
+        //tracking
+        public DateTime CreationDate { get; set; }
+        public string ModifiedDate { get; set; }
+        public User CreatedBy { get; set; }
+        public User ModifiedBy { get; set; }
+
         public TrainingPlan(string name, string description)
         {
             Name = name;
             Description = description;
             TrainingDays = new List<TrainingDay>();
+
+            this.CreationDate = DateTime.Now;
         }
 
         public TrainingPlan(int id, string name, string description)
@@ -19,6 +27,8 @@
             Name = name;
             Description = description;
             TrainingDays = new List<TrainingDay>();
+
+            this.CreationDate = DateTime.Now;
         }
 
     }
