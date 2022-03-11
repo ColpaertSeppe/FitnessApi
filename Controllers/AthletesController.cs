a27a7149fcd4771d65174f261a10724027c07fe8
Seppe <seppe.colpaert@student.hogent.be>
2022-03-11 10:32:25 +0100

    GetAthletes now uses azure db

--- Controllers/AthletesController.cs
+++ Controllers/AthletesController.cs
@@ -3,6 +3,7 @@ using FitnessApi.DTOs;
 using FitnessApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace FitnessApi.Controllers
@@ -12,26 +13,58 @@ namespace FitnessApi.Controllers
     public class AthletesController : ControllerBase
     {
         private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
 
-        public AthletesController(DataContext context)
+        public AthletesController(DataContext context, IConfiguration config)
         {
             _context = context;
+            _configuration = config;
         }
 
         //GET: api/Atheletes
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AthleteDTO.IndexAthlete>>> GetAtheletes()
         {
-            return await _context.Athletes.Select(x => new AthleteDTO.IndexAthlete
+            //return await _context.Athletes.Select(x => new AthleteDTO.IndexAthlete
+            //{
+            //    Id = x.Id,
+            //    FirstName = x.FirstName,
+            //    LastName = x.LastName,
+            //    Email = x.Email,
+            //    DateOfBirth = x.DateOfBirth,
+            //    Height = x.Height,
+            //    Weight = x.Weight,
+            //}).ToListAsync();
+
+            var athletes = new List<AthleteDTO.IndexAthlete>();
+
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("FitnessDB")))
             {
-                Id = x.Id,
-                FirstName = x.FirstName,
-                LastName = x.LastName,
-                Email = x.Email,
-                DateOfBirth = x.DateOfBirth,
-                Height = x.Height,
-                Weight = x.Weight,
-            }).ToListAsync();
+                var sql = "SELECT Id, FirstName, LastName, Email, DateOfBirth, Height, Weight FROM athlete";
+
+                connection.Open();
+                using SqlCommand command = new(sql, connection);
+                using SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var athlete = new AthleteDTO.IndexAthlete()
+                    {
+                        Id = (int)reader["ID"],
+                        FirstName = reader["FirstName"].ToString(),
+                        LastName = reader["LastName"].ToString(),
+                        Email = reader["Email"].ToString(),
+                        DateOfBirth = (DateTime)reader["DateOfBirth"],
+                        Height = (int)reader["Height"],
+                        Weight = (double)reader["Weight"],
+                    };
+                    athletes.Add(athlete);
+                }
+
+                connection.Close();
+            }
+
+                return athletes;
         }
 
         // GET: api/Atheletes/5
